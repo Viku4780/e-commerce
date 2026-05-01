68189c2f682454e12747d86c994b36e593fdc663
Viku4780 <vikrantmom5748@gmail.com>
2026-05-01 10:02:49 +0000

    rafactor layout

--- frontend/app/(user)/layout.jsx
+++ frontend/app/(user)/layout.jsx
@@ -1,18 +1,16 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import Header from "../components/Common/Header";
 import Footer from "../components/Common/Footer";
 import { Toaster } from "sonner";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "E-commerce",
   description: "the place from where you can buy the cloths of your choice",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}) {
   return (
     <>
       <Toaster position="top-right" />
--- frontend/app/admin/layout.jsx
+++ frontend/app/admin/layout.jsx
@@ -1,15 +1,13 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "E-commerce",
   description: "the place from where you can buy the cloths of your choice",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}) {
   return <>
     {children}
   </>;
